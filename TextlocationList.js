2f3b00de4403215c5e35f959ce1d9d54a2ca9133
lily <wulily7575@gmail.com>
2019-05-17 11:32:10 +0800

    0

---
+++ TextlocationList.js
@@ -0,0 +1,52 @@
+var TextlocationList = [{
+  "index": "0",
+  "pdfWidth": "0",
+  "pdfHeight": "0",
+  "content": [
+    {}
+  ]
+},{
+  "index": "0",
+  "pdfWidth": "0",
+  "pdfHeight": "0",
+  "content": [
+    {}
+  ]
+},{
+  "index": "0",
+  "pdfWidth": "0",
+  "pdfHeight": "0",
+  "content": [
+    {}
+  ]
+},{
+  "index": "0",
+  "pdfWidth": "841.89",
+  "pdfHeight": "595.276",
+  "content": [
+    {
+      "index": "0",
+      "width": "51.8400269",
+      "height": "46.0440025",
+      "left": "569.9067",
+      "top": "380.5739",
+      "keyword": "E"
+    },
+    {
+      "index": "1",
+      "width": "46.87201",
+      "height": "46.0440025",
+      "left": "621.7467",
+      "top": "380.5739",
+      "keyword": "y"
+    },
+    {
+      "index": "2",
+      "width": "48.0239868",
+      "height": "46.0440025",
+      "left": "668.6187",
+      "top": "380.5739",
+      "keyword": "e"
+    }
+  ]
+}];
